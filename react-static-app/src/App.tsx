6c3c56ae1649d090f2e9b379a6b9743c6e965be1
mfaridzia <farid19barcelona@gmail.com>
2020-02-19 03:37:32 +0700

    add react static app

---
+++ react-static-app/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React + Docker = 
+          <span role="img" aria-label="Rocket"> 🚀 </span>  
+          <span role="img" aria-label="Fire">  🔥 </span>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
